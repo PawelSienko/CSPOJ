4a1a3351e4bf0f894128cbadbac1ff210be30e1d
Pawel <sienko.pawel@gmail.com>
2021-12-05 22:04:39 +0100

    pole pwenego kola

---
+++ ETI06F1 - Pole pewnego koła/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETI06F1___Pole_pewnego_koła
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] args = Console.ReadLine().Split(' ');
+            double r = double.Parse(args[0]);
+            double d = double.Parse(args[1]);
+
+            double s = Math.PI * (Math.Pow(r, 2) - Math.Pow((d / 2), 2));
+
+            Console.Write(s);
+        }
+    }
+}
